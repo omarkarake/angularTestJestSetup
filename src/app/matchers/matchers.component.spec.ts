4a84770cb9d2ea07e74c452ca6ca1c850e03f080
omar karake <omar.karake@amalitechtraining.org>
2024-07-19 14:25:11 +0200

    different testing mechanism

--- src/app/matchers/matchers.component.spec.ts
+++ src/app/matchers/matchers.component.spec.ts
@@ -32,12 +32,43 @@ describe('MatchersComponent', () => {
   });
 
   // Truthiness
-  it('null', ()=>{
+  it('null', () => {
     const n = null;
-    expect(n).not.toBeNull();
+    // expect(n).not.toBeNull();
+    expect(n).toBeNull();
     // expect(n).toBeDefined();
     // expect(n).toBeUndefined();
     // expect(n).toBeTruthy();
     // expect(n).toBeFalsy();
-  })
+  });
+
+  // for floating numbers
+  it('adding floating numbers', () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+    expect(value).toBeGreaterThan(0.2);
+    expect(value).toBeLessThan(0.4);
+  });
+
+  // strings
+  it('there is no D in techops', () => {
+    expect('techopsworld').not.toMatch(/D/);
+  });
+
+  // strings
+  it('there is "ops" in techopsworld', () => {
+    expect('techopsworld').toMatch(/ops/);
+  });
+
+  it('the shopping list has milk on it', () => {
+    const shoppingList = [
+      'diapers',
+      'kleenes',
+      'trashbags',
+      'paper towels',
+      'milk',
+    ];
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+  });
 });
